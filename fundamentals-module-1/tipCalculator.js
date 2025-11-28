251d6fb1bf8e3979219800cf2e52c6e2188521d0
Victor Rey <rey08282014@outlook.com>
2025-11-28 16:32:06 -0500

    Cleaned up tipCalculator.js

--- fundamentals-module-1/tipCalculator.js
+++ fundamentals-module-1/tipCalculator.js
@@ -1,16 +1,7 @@
-// Simple tip calculator using functions and variables.
-
-// calculateTip(): receives a bill + tip percentage and returns the tip amount.
 function calculateTip(billAmount, tipPercent) {
-    return billAmount * (tipPercent / 100); // convert percentage + calculate tip.
+    const tip = billAmount * (tipPercent / 100);
+    const total = billAmount + tip;
+    return total;
 }
 
-// calculateTotal(): returns bill + calculated tip
-function calculateTotal(billAmount, tipPercent) {
-    const tip = calculateTip(billAmount, tipPercent); // reuses the calculateTip() function in the "tip" variable
-    return billAmount + tip; // adds bill + tip
-}
-
-//Tests function by logging results to the console.
-console.log(calculateTip(100, 20)); // should log: 20
-console.log(calculateTotal(100, 20)); // should log: 120
+console.log(calculateTip(50, 20))
